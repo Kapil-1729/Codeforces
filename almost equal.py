a823ab7ab533d74bcdf6f2df5eee32a9fc253fc2
Kapil-1729 <65090973+Kapil-1729@users.noreply.github.com>
2020-05-13 22:00:06 +0530

    Update almost equal.py

--- almost equal.py
+++ almost equal.py
@@ -1,19 +1,23 @@
 def almost_equal():
     n=int(input())
+    
     if n%2==0:
         print("NO")
         return
+    
     print("YES")
     for i in range(1,n+1):
         if i%2==0:
             print(2*i,end=' ')
         else:
             print((2*i)-1,end=' ')
+            
     for i in range(1,n+1):
         if i%2!=0:
             print(2*i,end=' ')
         else:
             print((2*i)-1,end=' ')
+            
     print('')
 
 almost_equal()
